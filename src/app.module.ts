a5a73a6cd12794b8bc68fdc8aeac9d26ea4f280e
Ingabire Divine <divineingabire69@gmail.com>
2025-04-12 16:06:53 +0200

    updates

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
 import { PrismaModule } from "./app/prisma/prisma.module";
-import { SharedModule } from "./app/shared/shared.module";
 import { UserModule } from "./app/user/user.module";
 import { AuthModule } from "./app/auth/auth.module";
 import { LocationModule } from "./app/location/location.module";
@@ -12,7 +11,6 @@ import { OperatorModule } from "./app/operator/operator.module";
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     PrismaModule,
-    SharedModule,
     UserModule,
     AuthModule,
     IncidentModule,
--- src/app/shared/shared.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module, Global } from "@nestjs/common";
-import { TwilioService } from "./twilio.service";
-
-@Global()
-@Module({
-  providers: [TwilioService],
-  exports: [TwilioService],
-})
-export class SharedModule {}
