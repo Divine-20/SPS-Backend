e2ad1a81667f1cb1b70fb4ce411da9b85946e808
Ingabire Divine <divineingabire69@gmail.com>
2025-04-23 11:25:41 +0200

    updates on app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,10 +7,16 @@ import { LocationModule } from "./app/location/location.module";
 import { IncidentModule } from "./app/case/incident.module";
 import { OperatorModule } from "./app/operator/operator.module";
 import { ServiceModule } from "./app/service/service.module";
+import { ServeStaticModule } from "@nestjs/serve-static";
+import { join } from "path";
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, "..", "uploads"),
+      serveRoot: "/incidents/images",
+    }),
     PrismaModule,
     UserModule,
     AuthModule,
